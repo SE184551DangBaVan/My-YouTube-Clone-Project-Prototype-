5cb33a8ca890fee3a41195c32bf394e98c528193
SE184551DangBaVan <VanDBSE184551@fpt.edu.vn>
2024-09-27 17:16:37 +0700

    Update api.ts leaks, oopsies

--- YouTube-Clone-Assignment/src/api.ts
+++ YouTube-Clone-Assignment/src/api.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 const request = axios.create({
     baseURL: "https://youtube.googleapis.com/youtube/v3/",
     params: {
-        key: "AIzaSyBdhJgCp1o4tjx_htd_nTVIPPQVjEaoZGE",//"AIzaSyBdhJgCp1o4tjx_htd_nTVIPPQVjEaoZGE",
+        key: import.meta.env.VITE_YT_API_KEY,
     },
 })
 
